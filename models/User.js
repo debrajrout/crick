5c47d98645675a92ea78e8a958d3c4a585c696c4
debrajrout <debrajrout28@gmail.com>
2025-05-17 21:03:56 +0530

    update dependencies: upgrade nodemon to version 3.1.10 and standardize code formatting in User model

--- models/User.js
+++ models/User.js
@@ -1,126 +1,134 @@
 // models/User.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const attendanceSchema = new Schema({
-    year: {
+  year: {
+    type: Number,
+    required: true,
+  },
+  months: [
+    {
+      month: {
         type: Number,
         required: true,
-    },
-    months: [{
-        month: {
+      },
+      days: [
+        {
+          day: {
             type: Number,
             required: true,
+          },
+          present: {
+            type: Boolean,
+            required: true,
+          },
         },
-        days: [{
-            day: {
-                type: Number,
-                required: true,
-            },
-            present: {
-                type: Boolean,
-                required: true,
-            }
-        }]
-    }]
+      ],
+    },
+  ],
 });
+//ss
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     name: {
-        type: String,
-        trim: true,
+      type: String,
+      trim: true,
     },
     firebaseUid: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     weight: {
-        type: Number,
+      type: Number,
     },
     height: {
-        type: Number,
+      type: Number,
     },
 
     education: {
-        type: String,
+      type: String,
     },
     referance: {
-        type: String,
+      type: String,
     },
 
     disease: {
-        type: String,
+      type: String,
     },
 
     injury: {
-        type: String,
+      type: String,
     },
 
     parrentName: {
-        type: String,
+      type: String,
     },
     occupation: {
-        type: String,
+      type: String,
     },
     address: {
-        type: String,
+      type: String,
     },
     remark: {
-        type: String,
+      type: String,
     },
 
     birthDate: {
-        type: Date,
+      type: Date,
     },
     mobileNumber: {
-        type: String,
-        unique: true,
-        trim: true,
-        required: true,
+      type: String,
+      unique: true,
+      trim: true,
+      required: true,
     },
     email: {
-        type: String,
+      type: String,
     },
     batch: {
-        type: Schema.Types.ObjectId,
-        ref: 'Batch',
+      type: Schema.Types.ObjectId,
+      ref: "Batch",
     },
     gender: {
-        type: String,
-        enum: ['Male', 'Female', 'Other'],
+      type: String,
+      enum: ["Male", "Female", "Other"],
     },
     role: {
-        type: String,
-        enum: ['Admin', 'Player', 'Coach'],
-        default: 'Player',
+      type: String,
+      enum: ["Admin", "Player", "Coach"],
+      default: "Player",
     },
     playerType: {
-        type: String,
-        enum: ['Batsman', 'Bowler', 'Wicketkeeper', 'Allrounder'],
+      type: String,
+      enum: ["Batsman", "Bowler", "Wicketkeeper", "Allrounder"],
     },
     attendance: [attendanceSchema],
     playedMatches: [
-        {
-            match: {
-                type: Schema.Types.ObjectId,
-                ref: 'Match',
-            },
-            result: {
-                type: String,
-                enum: ['Win', 'Lose'],
-            },
-        }
+      {
+        match: {
+          type: Schema.Types.ObjectId,
+          ref: "Match",
+        },
+        result: {
+          type: String,
+          enum: ["Win", "Lose"],
+        },
+      },
     ],
     upcomingMatches: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Match',
-        }
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Match",
+      },
     ],
-}, {
+  },
+  {
     timestamps: true,
-});
+  }
+);
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 module.exports = User;
